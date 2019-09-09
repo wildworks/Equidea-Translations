d6763ec19f6fea47452272f53a8b2a7a6988d8b3
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-09 15:03:24 +0200

    Add german color translations

---
+++ DE/colors.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Base colors
+    'COLOR_BLACK' => 'Rappe', // 1
+    'COLOR_SEAL_BROWN' => 'Schwarzbraun', // 2
+    'COLOR_BAY' => 'Braun', // 3
+    'COLOR_WILD_BAY' => 'Wildbraun', // 4
+    'COLOR_CHESTNUT' => 'Fuchs', // 5
+    'COLOR_FLAXEN_CHESTNUT' => 'Lichtfuchs', // 6
+
+    // Black color shades
+    'COLOR_LIGHT_BLACK' => 'Kohlrappe', // 7
+    'COLOR_DARK_BLACK' => 'Glanzrappe', // 8
+
+    // Sealbrown color shades
+    'COLOR_LIGHT_SEAL_BROWN' => 'Schwarzbraun', // 9
+    'COLOR_DARK_SEAL_BROWN' => 'Schwarzbraun', // 10
+
+    // Bay color shades
+    'COLOR_DARK_BAY' => 'Dunkelbraun', // 11
+    'COLOR_RED_BAY' => 'Rotbraun', // 12
+    'COLOR_LIGHT_BAY' => 'Hellbraun', // 13
+    'COLOR_COPPER_BAY' => 'Kastanienbraun', // 14
+    'COLOR_GOLD_BAY' => 'Goldbraun', // 15
+
+    // Wildbay color shades
+    'COLOR_DARK_WILD_BAY' => 'Dunkelbraun', // 16
+    'COLOR_RED_WILD_BAY' => 'Wildbraun', // 17
+    'COLOR_LIGHT_WILD_BAY' => 'Hellbraun', // 18
+    'COLOR_COPPER_WILD_BAY' => 'Wildbraun', // 19
+    'COLOR_GOLD_WILD_BAY' => 'Goldbraun', // 20
+
+    // Chestnut color shades
+    'COLOR_DARK_CHESTNUT' => 'Dunkelfuchs', // 21
+    'COLOR_COAL_CHESTNUT' => 'Kohlfuchs', // 22
+    'COLOR_RED_CHESTNUT' => 'Rotfuchs', // 23
+    'COLOR_LIGHT_CHESTNUT' => 'Hellfuchs', // 24
+    'COLOR_COPPER_CHESTNUT' => 'Kupferfuchs', // 25
+    'COLOR_GOLD_CHESTNUT' => 'Goldfuchs', // 26
+
+    // Flaxen chestnut color shades
+    'COLOR_DARK_FLAXEN_CHESTNUT' => 'Dunkellichtfuchs', // 27
+    'COLOR_COAL_FLAXEN_CHESTNUT' => 'Kohllichtfuchs', // 28
+    'COLOR_RED_FLAXEN_CHESTNUT' => 'Lichtfuchs', // 29
+    'COLOR_LIGHT_FLAXEN_CHESTNUT' => 'Lichtfuchs', // 30
+    'COLOR_COPPER_FLAXEN_CHESTNUT' => 'Lichtfuchs', // 31
+    'COLOR_GOLD_FLAXEN_CHESTNUT' => 'Lichtfuchs', // 32
+
+    // Base colors with single cream
+    'COLOR_SMOKY_BLACK' => 'Erdbraun', // 33
+    'COLOR_SMOKY_BROWN' => 'Erdbraun', // 34
+    'COLOR_BUCKSKIN' => 'Erdfarben', // 35
+    'COLOR_WILD_BUCKSKIN' => 'Erdfarben', // 36
+    'COLOR_PALOMINO' => 'Isabell', // 37
+    // As hair is already white, flaxen doesn't have any effect
+
+    // Base colors with double cream
+    'COLOR_BLACK_SMOKY_CREAM' => 'Smoky Cream', // 38
+    'COLOR_BROWN_SMOKY_CREAM' => 'Smoky Cream', // 39
+    'COLOR_PERLINO' => 'Perlino', // 40
+    'COLOR_WILD_PERLINO' => 'Perlino', // 41
+    'COLOR_CREMELLO' => 'Cremello', // 42
+    // As hair is already white, flaxen doesn't have any effect on palomino
+
+];
