7667ba9badfa868fa82a3bc9bd1013d711be3853
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-09 15:31:48 +0200

    Add italian grey translations

---
+++ IT/greys.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Dark grey horses with a black or seal brown base
+    'COLOR_BLACK_DAPPLE_GREY' => 'Grigio pomellato', // 1
+    'COLOR_BLACK_FLEABITTEN_GREY' => '???', // 2
+    'COLOR_DARK_GREY' => 'Grigio', // 3
+
+    // Bay grey horses with any bay base
+    'COLOR_BAY_DAPPLE_GREY' => 'Grigio pomellato', // 4
+    'COLOR_BAY_FLEABITTEN_GREY' => '???', // 5
+    // Solid bay grey same as solid black grey
+
+    // Chestnut grey horses with any non flaxen chestnut base
+    'COLOR_CHESTNUT_DAPPLE_GREY' => 'Grigio pomellato', // 6
+    'COLOR_RED_FLEABITTEN_GREY' => '???', // 7
+    'COLOR_RED_SOLID_GREY' => 'Grigio', // 8
+
+    // Flaxen chestnut grey horses with any flaxen base
+    'COLOR_FLAXEN_DAPPLE_GREY' => 'Grigio pomellato', // 9
+    // Solid and fleabitten chestnut grey same as solid black grey
+
+    // Some single cream colored grey horses will have golden dapples
+    'COLOR_BAY_GOLD_DAPPLE_GREY' => 'Grigio pomellato', // 10
+    'COLOR_RED_GOLD_DAPPLE_GREY' => 'Grigio pomellato', // 11
+    'COLOR_GOLD_FLEABITTEN_GREY' => '???', // 12
+    // Solid grey single cream horses show no difference to non-creams
+    
+    // Double cream horses
+    'COLOR_CREAM_GREY' => 'Grigio' // 13
+    // Only smoky cream will be classified as grey
+
+];
