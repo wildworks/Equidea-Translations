bab9d9235e7a07a3a546a0f026cbaf6b75c2bab7
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-09 15:27:21 +0200

    Add italian color translations

---
+++ IT/colors.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Base colors
+    'COLOR_BLACK' => 'Morello', // 1
+    'COLOR_SEAL_BROWN' => '???', // 2
+    'COLOR_BAY' => 'Baio', // 3
+    'COLOR_WILD_BAY' => '???', // 4
+    'COLOR_CHESTNUT' => 'Sauro', // 5
+    'COLOR_FLAXEN_CHESTNUT' => '???', // 6
+
+    // Black color shades
+    'COLOR_LIGHT_BLACK' => '???', // 7
+    'COLOR_DARK_BLACK' => '???', // 8
+
+    // Sealbrown color shades
+    'COLOR_LIGHT_SEAL_BROWN' => '???', // 9
+    'COLOR_DARK_SEAL_BROWN' => '???', // 10
+
+    // Bay color shades
+    'COLOR_DARK_BAY' => '???', // 11
+    'COLOR_RED_BAY' => '???', // 12
+    'COLOR_LIGHT_BAY' => '???', // 13
+    'COLOR_COPPER_BAY' => '???', // 14
+    'COLOR_GOLD_BAY' => '???', // 15
+
+    // Wildbay color shades
+    'COLOR_DARK_WILD_BAY' => '???', // 16
+    'COLOR_RED_WILD_BAY' => '???', // 17
+    'COLOR_LIGHT_WILD_BAY' => '???', // 18
+    'COLOR_COPPER_WILD_BAY' => '???', // 19
+    'COLOR_GOLD_WILD_BAY' => '???', // 20
+
+    // Chestnut color shades
+    'COLOR_DARK_CHESTNUT' => '???', // 21
+    'COLOR_COAL_CHESTNUT' => '???', // 22
+    'COLOR_RED_CHESTNUT' => '???', // 23
+    'COLOR_LIGHT_CHESTNUT' => '???', // 24
+    'COLOR_COPPER_CHESTNUT' => '???', // 25
+    'COLOR_GOLD_CHESTNUT' => '???', // 26
+
+    // Flaxen chestnut color shades
+    'COLOR_DARK_FLAXEN_CHESTNUT' => '???', // 27
+    'COLOR_COAL_FLAXEN_CHESTNUT' => '???', // 28
+    'COLOR_RED_FLAXEN_CHESTNUT' => '???', // 29
+    'COLOR_LIGHT_FLAXEN_CHESTNUT' => '???', // 30
+    'COLOR_COPPER_FLAXEN_CHESTNUT' => '???', // 31
+    'COLOR_GOLD_FLAXEN_CHESTNUT' => '???', // 32
+
+    // Base colors with single cream
+    'COLOR_SMOKY_BLACK' => '???', // 33
+    'COLOR_SMOKY_BROWN' => '???', // 34
+    'COLOR_BUCKSKIN' => '???', // 35
+    'COLOR_WILD_BUCKSKIN' => '???', // 36
+    'COLOR_PALOMINO' => '???', // 37
+    // As hair is already white, flaxen doesn't have any effect
+
+    // Base colors with double cream
+    'COLOR_BLACK_SMOKY_CREAM' => '???', // 38
+    'COLOR_BROWN_SMOKY_CREAM' => '???', // 39
+    'COLOR_PERLINO' => '???', // 40
+    'COLOR_WILD_PERLINO' => '???', // 41
+    'COLOR_CREMELLO' => '???', // 42
+    // As hair is already white, flaxen doesn't have any effect on palomino
+
+];
