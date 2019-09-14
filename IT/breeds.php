0b549d23bf42180c35be5616a638c03c348e0eff
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-14 20:41:23 +0200

    Add italian breed translations

---
+++ IT/breeds.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Label for profiles and tables
+    'LABEL_BREED' => 'Razza',
+
+    // National breeds
+    'BREED_GERMAN_WARMBLOOD' => '???', // 1
+    'BREED_AUSTRIAN_WARMBLOOD' => '???', // 2
+    'BREED_SWISS_WARMBLOOD' => '???', // 3
+    'BREED_ITALIAN_WARMBLOOD' => '???', // 4
+
+    // German open breeds
+    'BREED_HANOVERIAN' => '???', // 5
+    'BREED_HOLSTEINER' => '???', // 6
+    'BREED_OLDENBURGER' => '???', // 7
+    'BREED_TRAKEHNER' => '???', // 8
+    
+    // Austrian open breeds
+    'BREED_HAFLINGER' => '???' // 9
+
+];
