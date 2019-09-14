4ecac03cc3aeea48e5768e67e7233074c58e8d38
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-14 13:58:35 +0200

    Add italian horse translations

---
+++ IT/horses.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Horse profile labels
+    'LABEL_AGE' => 'Età',
+    'LABEL_GENDER' => 'Sesso',
+    'LABEL_OWNER' => '???',
+
+    // Horse heritage label
+    'LABEL_BREEDER' => 'Produttore',
+    'LABEL_MOTHER' => 'Madre',
+    'LABEL_FATHER' => 'Padre',
+
+    // Age translations
+    'AGE_NEWBORN' => '???',
+    'AGE_MONTH' => 'meso',
+    'AGE_MONTHS' => 'mesi',
+    'AGE_YEAR' => 'anno',
+    'AGE_YEARS' => 'anni',
+
+    // Gender translations
+    'GENDER_STALLION' => '???', // 1
+    'GENDER_MARE' => '???', // 2
+
+    // Unknown value fallback
+    'VALUE_UNKNOWN' => '???',
+    
+    // Horse name fallbacks
+    'HORSE_UNNAMED' => '???'
+
+];
