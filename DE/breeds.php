d8637084debf1137200784f9c5d03a142668fb48
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-14 16:03:36 +0200

    Add german breed translations

---
+++ DE/breeds.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Label for profiles and tables
+    'LABEL_BREED' => 'Rasse',
+
+    // National breeds
+    'BREED_GERMAN_WARMBLOOD' => 'Deutsches Warmblut', // 1
+    'BREED_AUSTRIAN_WARMBLOOD' => 'Österreichisches Warmblut', // 2
+    'BREED_SWISS_WARMBLOOD' => 'Schweizer Warmblut', // 3
+    'BREED_ITALIAN_WARMBLOOD' => 'Italienisches Warmblut', // 4
+
+    // German open breeds
+    'BREED_HANOVERIAN' => 'Hannoveraner', // 5
+    'BREED_HOLSTEINER' => 'Holsteiner', // 6
+    'BREED_OLDENBURGER' => 'Oldenburger', // 7
+    'BREED_TRAKEHNER' => 'Trakehner', // 8
+    
+    // Austrian open breeds
+    'BREED_HAFLINGER' => 'Haflinger' // 9
+
+];
