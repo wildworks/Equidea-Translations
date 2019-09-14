cb21e243e2481f31abfec50758ce870784ba5d8c
wildworks <50334572+wildworks@users.noreply.github.com>
2019-09-14 13:48:36 +0200

    German horse profile translation

---
+++ DE/horses.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * THIS FILE IS PART OF THE EQUIDEA BROWSERGAME.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Horse profile labels
+    'LABEL_AGE' => 'Alter',
+    'LABEL_GENDER' => 'Geschlecht',
+    'LABEL_OWNER' => 'Besitzer',
+
+    // Horse heritage label
+    'LABEL_BREEDER' => 'Züchter',
+    'LABEL_MOTHER' => 'Mutter',
+    'LABEL_FATHER' => 'Vater',
+
+    // Age translations
+    'AGE_NEWBORN' => 'Wenige Stunden',
+    'AGE_MONTH' => 'Monat',
+    'AGE_MONTHS' => 'Monate',
+    'AGE_YEAR' => 'Jahr',
+    'AGE_YEARS' => 'Jahre',
+
+    // Gender translations
+    'GENDER_STALLION' => 'Hengst', // 1
+    'GENDER_MARE' => 'Stute', // 2
+
+    // Unknown value fallback
+    'VALUE_UNKNOWN' => 'Unbekannt',
+    
+    // Horse name fallbacks
+    'HORSE_UNNAMED' => 'Unbenannt'
+
+];
